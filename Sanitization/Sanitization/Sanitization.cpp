3ba269a5aa393d781e0bccee87c8b49752cc1b0a
Matthew McGuff <matthewmcguff85@gmail.com>
2018-01-23 21:25:45 -0700

    Starting from hello world

---
+++ Sanitization/Sanitization/Sanitization.cpp
@@ -0,0 +1,21 @@
+// Sanitization.cpp : main project file.
+
+#include "stdafx.h"
+#include <iostream>
+
+
+using namespace System;
+using namespace std;
+
+int main()
+{
+	cout << "Hello, World!";
+	
+	getchar();
+	return 0;
+}
+
+
+
+
+
