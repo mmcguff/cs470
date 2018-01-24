43ecc2834fac152b6bc068dab68d8a40449b6334
Matthew McGuff <matthewmcguff85@gmail.com>
2018-01-23 21:44:09 -0700

    program takes input form the screen and outputs it to the screen

--- Sanitization/Sanitization/Sanitization.cpp
+++ Sanitization/Sanitization/Sanitization.cpp
@@ -2,20 +2,22 @@
 
 #include "stdafx.h"
 #include <iostream>
-
+#include <string>
 
 using namespace System;
 using namespace std;
 
 int main()
 {
-	cout << "Hello, World!";
+	string input;
+
+	cout << "Please enter HTML to validate"; cout << endl;
+	
+	cin >> input;
+	getchar();
+	cout << "You typed : " + input;
 	
 	getchar();
+	
 	return 0;
 }
-
-
-
-
-
