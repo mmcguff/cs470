a1449e46cbd17d6e1728cae3aa66e8a9ff6d303a
Matthew McGuff <matthewmcguff85@gmail.com>
2018-01-23 22:00:14 -0700

    Now I have added a bool function.  Know we just need to put the logic in place to loop through a string and print true if items contained inside tags is valid.

--- Sanitization/Sanitization/Sanitization.cpp
+++ Sanitization/Sanitization/Sanitization.cpp
@@ -7,17 +7,31 @@
 using namespace System;
 using namespace std;
 
+bool isValid(string targetString)
+{
+	//We should have the logic here to deteremine if the HTML tag is valid.  
+	return true;
+}
+
+
+
 int main()
 {
 	string input;
-
+	bool ans;
 	cout << "Please enter HTML to validate"; cout << endl;
 	
 	cin >> input;
 	getchar();
-	cout << "You typed : " + input;
 	
+	ans = isValid(input);
+	
+	cout << "You input is " + to_string(ans);
+	
+
+
 	getchar();
 	
 	return 0;
 }
+
