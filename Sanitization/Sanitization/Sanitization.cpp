83811aa7e056eaf7ed55afd46dec27fcc31875fa
Matthew McGuff <matthewmcguff85@gmail.com>
2018-01-29 06:40:27 -0700

    I have some logic now showing where the opening tag < and closing > come up in the input string.  I have a variable tag that is getting the value of the tag.  Now we should just add logic to ignore after the first space andsee if the tag matches are white list of tags in the assignment.

--- Sanitization/Sanitization/Sanitization.cpp
+++ Sanitization/Sanitization/Sanitization.cpp
@@ -9,7 +9,32 @@ using namespace std;
 
 bool isValid(string targetString)
 {
+	string tag;
+	bool isTag = false;
 	//We should have the logic here to deteremine if the HTML tag is valid.  
+	for (std::string::size_type i = 0; i < targetString.size(); ++i) {
+		if (targetString[i] == '<')
+		{
+			cout << "< is a opening tag\n";
+			isTag = true;
+			continue;
+		}
+		if (targetString[i] == '>')
+		{
+			cout << "> is a closing tag\n";
+			isTag = false;
+			continue;
+
+		}
+		if (isTag == true)
+		{
+			tag += targetString[i];
+			cout << tag; cout << endl;
+		}
+
+
+		//cout << targetString[i]; cout << endl;
+	}
 	return true;
 }
 
@@ -19,19 +44,22 @@ int main()
 {
 	string input;
 	bool ans;
-	cout << "Please enter HTML to validate"; cout << endl;
+	cout << "Please enter HTML to validate\n";
 	
 	cin >> input;
 	getchar();
 	
 	ans = isValid(input);
 	
-	cout << "You input is " + to_string(ans);
-	
-
-
-	getchar();
-	
+	if (ans == true)
+	{
+		cout << "You input is valid\n";
+	}
+	else if (ans == false)
+	{
+		cout << "Your input is invalid\n";
+	}
+		
 	return 0;
 }
 
